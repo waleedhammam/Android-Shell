e3319a132b1b48d1448e223ad18a1086f166fa23
Waleed Hammam <Waleed.Hammam@gmail.com>
2018-05-30 02:08:46 +0200

    Changelog: _ Tweaks to code _ Tweaks to output style _ Added Clear screen button _ Added scrollbars to output

--- app/src/main/java/com/hammam/shell/MainActivity.java
+++ app/src/main/java/com/hammam/shell/MainActivity.java
@@ -2,6 +2,7 @@ package com.hammam.shell;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,28 +13,38 @@ import com.jrummyapps.android.shell.Shell;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText input;
-    Button btn;
-    TextView out;
+    Button execute, clear;
+    EditText inputCommand;
+    TextView outputResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        input = findViewById(R.id.txt);
-        btn = findViewById(R.id.btn);
-        out = findViewById(R.id.out);
-        btn.setOnClickListener(new View.OnClickListener() {
+        execute = findViewById(R.id.execute_button);
+        clear = findViewById(R.id.clear_button);
+
+        inputCommand = findViewById(R.id.input_text);
+        outputResult = findViewById(R.id.output_text);
+        outputResult.setMovementMethod(new ScrollingMovementMethod());
+
+        execute.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
-                CommandResult result = Shell.SH.run(input.getText().toString());
-                out.append("input\n" + input.getText().toString() + "\n" +
-                        "Output\n" + result.toString() + "\n");
+                CommandResult result = Shell.SH.run(inputCommand.getText().toString());
+                outputResult.append("$ " + inputCommand.getText().toString() + "\n" +
+                        "> " + result.toString() + "\n");
             }
         });
 
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                outputResult.setText("");
+            }
+        });
     }
 
 }
